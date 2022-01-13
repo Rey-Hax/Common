5feb3114dd0752fab017f31558fa168388c47c09
Rey-Hax <reyhax@protonmail.com>
2022-01-13 13:59:12 -0800

    Removed comments

--- Editor/CommonDefines.cs
+++ Editor/CommonDefines.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
-using UnityEngine;
-
 namespace PhantasmicGames.CommonEditor
 {
 	public class CommonDefines : CustomDefines
@@ -18,23 +12,5 @@ namespace PhantasmicGames.CommonEditor
 				condition = () => true,
 			}
 		};
-
-		//internal class CustomAssetModificationProcessor : UnityEditor.AssetModificationProcessor
-		//{
-		//	private static readonly DirectoryInfo s_ScriptDirectoryInfo = new DirectoryInfo(GetScriptPath());
-
-		//	static AssetDeleteResult OnWillDeleteAsset(string assetName, RemoveAssetOptions options)
-		//	{
-		//		var assetDir = new DirectoryInfo(assetName);
-
-		//		Debug.Log("Asset: " + assetDir.FullName);
-		//		Debug.Log("Script: " + s_ScriptDirectoryInfo.FullName);
-
-		//		return AssetDeleteResult.DidNotDelete;
-		//	}
-
-		//	private static string GetScriptPath([System.Runtime.CompilerServices.CallerFilePath] string fileName = null) => fileName;
-		//}
-
 	}
 }
