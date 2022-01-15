2f2ead894e3edb1240b02cdc674119b7df86a6b5
Rey-Hax <reyhax@protonmail.com>
2022-01-15 10:55:13 -0800

    Moved editor logic into the runtime script

--- Runtime/Attributes/ReadOnlyAttribute.cs
+++ Runtime/Attributes/ReadOnlyAttribute.cs
@@ -1,21 +1,40 @@
 using System;
 using UnityEngine;
 
-namespace PhantasmicGames.Common
-{
-	/// <summary>
-	/// Make the field unable to be changed from the editor.
-	/// Does not work as expected with variable types that have a custom PropertyDrawer.
-	/// </summary>
-	[AttributeUsage(AttributeTargets.Field)]
-	public class ReadOnlyAttribute : PropertyAttribute
-	{
-		public bool playModeOnly;
+#if UNITY_EDITOR
+using UnityEditor;
 
-		/// <param name="playModeOnly">Set true to disallow editing field while editor is in playmode.</param>
-		public ReadOnlyAttribute(bool playModeOnly = false)
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		var enabled = (attribute as ReadOnlyAttribute).playModeOnly && !Application.isPlaying;
+		using (var scope = new EditorGUI.DisabledGroupScope(!enabled))
 		{
-			this.playModeOnly = playModeOnly;
+			EditorGUI.PropertyField(position, property, label, true);
 		}
+	}
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return EditorGUI.GetPropertyHeight(property, label, true);
+	}
+}
+#endif
+
+/// <summary>
+/// Make the field unable to be changed from the editor.
+/// Does not work as expected with variable types that have a custom PropertyDrawer.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field)]
+public class ReadOnlyAttribute : PropertyAttribute
+{
+	public bool playModeOnly;
+
+	/// <param name="playModeOnly">Set true to disallow editing field while editor is in playmode.</param>
+	public ReadOnlyAttribute(bool playModeOnly = false)
+	{
+		this.playModeOnly = playModeOnly;
 	}
 }
--- Runtime/Attributes/ReadOnlyCollectionAttribute.cs
+++ Runtime/Attributes/ReadOnlyCollectionAttribute.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 
-namespace PhantasmicGames.Common
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyCollectionAttribute))]
+public class ReadOnlyCollectionDrawer : DecoratorDrawer
 {
-	/// <summary>
-	/// Use for fields like Arrays or Lists to remove the ability to edit the collection in the editor.
-	/// </summary>
-	[AttributeUsage(AttributeTargets.Field)]
-	public class ReadOnlyCollectionAttribute : ReadOnlyAttribute
+	public override void OnGUI(Rect position)
 	{
-		/// <param name="playModeOnly">Set true to disallow editing field while editor is in playmode.</param>
-		public ReadOnlyCollectionAttribute(bool playModeOnly = false) : base(playModeOnly)
-		{
-		}
+		var enabled = (attribute as ReadOnlyAttribute).playModeOnly && !Application.isPlaying;
+		if (!enabled)
+			EditorGUI.BeginDisabledGroup(true);
+	}
+
+	public override float GetHeight()
+	{
+		return 0f;
+	}
+}
+#endif
+
+/// <summary>
+/// Use for fields like Arrays or Lists to remove the ability to edit the collection in the editor.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field)]
+public class ReadOnlyCollectionAttribute : ReadOnlyAttribute
+{
+	/// <param name="playModeOnly">Set true to disallow editing field while editor is in playmode.</param>
+	public ReadOnlyCollectionAttribute(bool playModeOnly = false) : base(playModeOnly)
+	{
 	}
 }
